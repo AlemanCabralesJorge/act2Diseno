da2b007e28fd63b329459cc1ffdfe92db9e8a37d
JORGE_ALEMAN_CABRALES <a.20308051280480@cbtis128.edu.mx>
2023-05-16 16:39:21 +0000

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,153 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const AppLiverpool());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppLiverpool extends StatelessWidget {
+  const AppLiverpool({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PaginaDeInicio(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class PaginaDeInicio extends StatelessWidget {
+  const PaginaDeInicio({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor:
+            const Color(0xFFD40086), // Color personalizado para el AppBar
+        title: const Text('Liverpool'),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xFFD40086),
+              ),
+              child: Text(
+                'Liverpool',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Inicio'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const PaginaDeInicio()),
+                ); // Acción al hacer clic en la opción del drawer
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Configuración'),
+              onTap: () {
+                // Acción al hacer clic en la opción del drawer
+              },
+            ),
+          ],
+        ),
+      ),
+      body: DefaultTabController(
+        length: 3,
+        child: Column(
+          children: <Widget>[
+            const TabBar(
+              indicatorColor: Color(0xFFD40086),
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.people, color: Colors.black)),
+                Tab(icon: Icon(Icons.phone_android, color: Colors.black)),
+                Tab(icon: Icon(Icons.settings, color: Colors.black)),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: <Widget>[
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 250,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 3),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              'Alemán Cabrales Jorge',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        Container(
+                          width: 150,
+                          height: 0,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 5),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        const CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(
+                            'https://raw.githubusercontent.com/AlemanCabralesJorge/liverpool_img/main/yo.jpg',
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          width: 150,
+                          height: 0,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 5),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        Container(
+                          width: 250,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(color: Colors.black, width: 3),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              '6 - I | Programación',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Center(
+                    child: Text('Celulares'),
+                  ),
+                  const Center(
+                    child: Text('Configuración'),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
